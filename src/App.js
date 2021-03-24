a16f792a116119d3aebc715e815459748704e516
bim240 <bimlendu357@gmail.com>
2021-03-24 23:41:01 +0530

    theme setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "utils/Styles/GlobalStyles";
+import { lightTheme, darkTheme } from "utils/Styles/Themes";
+
 function App() {
-  return <div className="App">hello</div>;
+  const [theme, setTheme] = useState("dark");
+  return (
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <div className="App">hello</div>;
+      <button
+        type="primary"
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
+        {" "}
+        toggle
+      </button>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/utils/Styles/Themes.js
@@ -0,0 +1,10 @@
+export const lightTheme = {
+  body: "#dcdcdc",
+  text: "#2c2c2c",
+  background: "#dcdcdc",
+};
+export const darkTheme = {
+  body: "#2c2c2c",
+  text: "#dcdcdc",
+  background: "#999",
+};
