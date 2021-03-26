0e6b838c67c6833a3395dedf949c9d804bedbb2b
bim240 <bimlendu357@gmail.com>
2021-03-26 16:27:35 +0530

    responsive nav bar

--- src/Components/Header/StyledComponents.js
+++ src/Components/Header/StyledComponents.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const NavBarWrapper = styled.section`
+export const HeaderWrapper = styled.section`
   width: 100%;
   height: 4rem;
   background: ${(props) => props.theme.secondaryBackground};
@@ -26,6 +26,50 @@ export const NavBarWrapper = styled.section`
       padding: 0;
       font-size: 2rem;
       font-weight: 900;
+    }
+  }
+  .toggle_btn {
+    margin-right: 1rem;
+    color: ${(props) => props.theme.text};
+    background: ${(props) => props.theme.secondaryBackground};
+  }
+  .header_navbar {
+    width: 300px;
+    display: none;
+  }
+  .absolute_navbar {
+    position: absolute;
+    top: 4rem;
+    left: 0;
+  }
+  @media (max-width: 1000px) {
+    .container {
+      width: 100%;
+      padding: 1rem;
+    }
+    img {
+      display: none;
+    }
+    .header_navbar {
+      display: block;
+    }
+  }
+
+  @media (min-width: 1000.1px) {
+    .container {
+      padding: 0rem;
+    }
+    img {
+      display: block;
+    }
+    .header_navbar {
+      display: block;
+    }
+    .toggle_btn {
+      display: none;
+    }
+    .header_navbar {
+      display: none;
     }
   }
 `;
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,18 +1,32 @@
-import { Switch } from "antd";
-import { NavBarWrapper } from "./StyledComponents";
+import { Button, Switch } from "antd";
+import { useState } from "react";
+import { HeaderWrapper } from "./StyledComponents";
+import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
+import NavBar from "Components/NavBar";
 
 const Header = (props) => {
   const { toggleTheme } = props;
+
+  const [showNavBar, setShowNavBar] = useState(false);
+
   return (
-    <NavBarWrapper>
+    <HeaderWrapper>
       <div className="container">
         <div className="logo_title">
+          <Button
+            className="toggle_btn"
+            onClick={() => setShowNavBar(!showNavBar)}>
+            {showNavBar ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+          </Button>
           <img src="https://cdn2.iconfinder.com/data/icons/flat-education-icons-5/96/Writing-512.png" />
           <h1>My Writings</h1>
         </div>
         <Switch onChange={toggleTheme} />
       </div>
-    </NavBarWrapper>
+      <div className="absolute_navbar">
+        {showNavBar && <NavBar className="header_navbar" />}
+      </div>
+    </HeaderWrapper>
   );
 };
 
