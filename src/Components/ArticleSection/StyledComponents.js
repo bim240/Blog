9064f9cc517cf70ff903d38053c9c5bb5e84bbe4
bim240 <bimlendu357@gmail.com>
2021-03-27 17:20:35 +0530

    take care of fixed footer

--- src/Components/ArticleSection/StyledComponents.js
+++ src/Components/ArticleSection/StyledComponents.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 export const StyledArticleSection = styled.section`
   width: 90%;
   margin: 1rem auto;
+  margin-bottom: 3rem;
   display: grid;
   grid-template-columns: 1fr 3fr;
   column-gap: 2rem;
